dd3885755a6f45c6a290a8ba513f9e8f8081f4ed
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-27 19:30:28 +0000

    Apollo server and running API

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require("./schema/type-defs");
+const { resolvers } = require("./schema/type-defs");
 
-const server = new ApolloServer();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`YOUR API IS RUNNING AT: ${url}`);
+});
